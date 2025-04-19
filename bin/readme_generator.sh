83896280ee635c4795230e25451e6cadb1431d10
Aaron VerDow <aaron@verdow.com>
2025-04-19 15:42:57 -0500

    removing old files

--- bin/readme_generator.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-# quick and dirty version
-
-for gif in `ls | grep gif | sed 's/\.gif//'`; do
-    echo "## $gif"
-    echo "![](https://raw.githubusercontent.com/AaronVerDow/$1/master/animations/output/$gif.gif)"
-done
